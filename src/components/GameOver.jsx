68b35ce145d1709b309bf09b7f49002893659540
Big Chamel <ali10.imkamel@gmail.com>
2022-11-25 13:32:33 +0000

    deploy

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -3,12 +3,12 @@ import { AppContext } from "./App"
 
 
 function GameOver({ winner }) {
-    
+
     return (
         <div className='game-over'>
             <h1>Game Over</h1>
             {winner !== "" ? <h2>Winner: {winner.toUpperCase()}</h2> : <h2>No Winner</h2>}
-            <a href=""><button className='btn'>Play Again</button></a>
+            <a href="/tic-tac-toe"><button className='btn'>Play Again</button></a>
         </div>
     )
 }
